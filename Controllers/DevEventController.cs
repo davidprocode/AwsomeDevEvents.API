ed8aeb17cb4416a2f64e8d4a488adbb20eb2c3db
David Santos <davidprocode@proton.me>
2023-10-02 21:50:18 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/DevEventController.cs
@@ -0,0 +1,49 @@
+﻿using AwsomeDevEvents.API.Entities;
+using AwsomeDevEvents.API.Persistence;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AwsomeDevEvents.API.Controllers
+{
+    [Route("api/[dev-events]")]
+    [ApiController]
+    public class DevEventController : ControllerBase
+    {
+        private readonly DevEventDbContext _dbContext;
+        public DevEventController(DevEventDbContext context ) 
+        {
+            _Dbcontext = context;
+        }
+        [HttpGet]
+        public IActionResult GetAll() 
+        {
+            var devEvents = _dbContext.DevEvents.Where(d => d.IsDeleted).ToList();
+            return Ok(devEvents);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid Id)
+        {
+            var devEvents = _dbContext.DevEvents.SingleOrDefault(d => d.Id == Id);
+            if (devEvent == null)
+            {
+                return NotFound();
+            }
+            return Ok(devEvents);
+
+        }
+        [HttpGet]
+        public IActionResult Post(DevEvent devEvent)
+        {
+
+        }
+        [HttpGet("{id}")]
+        public IActionResult Update(Guid id, DevEvent devEvent)
+        {
+
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id) 
+        {
+        }
+    }
+}
---
+++ Persistence/DevEventDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AwsomeDevEvents.API.Entities;
+
+namespace AwsomeDevEvents.API.Persistence
+{
+    public class DevEventDbContext
+    {
+        public List<DevEvent> DevEvents { get; set; }
+        public DevEventDbContext()
+        {
+            DevEvents = new List<DevEvent>();
+        }
+    }
+}
